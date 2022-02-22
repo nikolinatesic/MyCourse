10af78b2cd172a8f4d9cc30ebe82aaed97389c1d
Nikolina Tesic <97803513+nikolinatesic@users.noreply.github.com>
2022-02-22 00:41:23 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+//COUNTDOWN
+var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+//FAQ
+
+let summaryCollection = document.getElementsByTagName('summary');
+let signsCollection = document.getElementsByClassName('faq-open-icon');
+
+for(let i = 0; i < summaryCollection.length; i++) {
+  summaryCollection[i].onclick = function() {
+    if(signsCollection[i].innerHTML === '+') signsCollection[i].innerHTML = '—';
+    else signsCollection[i].innerHTML = '+';
+  }
+}
